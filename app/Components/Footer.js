398dfa028ef8cdbb25a8a5dab9201621ee84e87a
Aniket Saha <aniketsahaworkspace@gmail.com>
2025-02-22 19:38:46 +0530

    Enhance global CSS and Footer responsiveness with mobile-friendly adjustments

--- app/Components/Footer.js
+++ app/Components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
   return (
     <>
       <div className="text-white flex justify-evenly min-h-[400px] pt-32">
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3  md:block hidden">
           <span className="text-white/70 text-base">VER A.S.W</span>
           <div>
             <Link
@@ -24,8 +24,8 @@ const Footer = () => {
           </div>
         </div>
 
-        <nav className="flex flex-col gap-3">
-          <span className="pb-2 text-white/70 text-base font-medium">SOCIALS</span>
+        <nav className="flex flex-col gap-3 md:block hidden">
+          <span className="pb-3 text-white/70 text-base font-medium">SOCIALS</span>
           <div>
             <Link
               href="https://www.linkedin.com/in/aniketsaha2005"
@@ -33,8 +33,8 @@ const Footer = () => {
               rel="noopener noreferrer"
               className="group flex items-center gap-2 text-lg"
             >
-              <FaLinkedin className="text-xl group-hover:text-blue-600 transition-colors" />
-              <span className="relative inline-block after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
+              <FaLinkedin className="text-xl my-1 group-hover:text-blue-600 transition-colors" />
+              <span className="hidden md:inline-block relative inline-block after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
                 LinkedIn
               </span>
             </Link>
@@ -46,8 +46,8 @@ const Footer = () => {
               rel="noopener noreferrer"
               className="group flex items-center gap-2 text-lg"
             >
-              <FaGithub className="text-xl group-hover:text-gray-600 transition-colors" />
-              <span className="relative inline-block after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
+              <FaGithub className="text-xl my-1 group-hover:text-gray-600 transition-colors" />
+              <span className="hidden md:inline-block relative inline-block after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
                 GitHub
               </span>
             </Link>
@@ -59,8 +59,8 @@ const Footer = () => {
               rel="noopener noreferrer"
               className="group flex items-center gap-2 text-lg"
             >
-              <FaInstagram className="text-xl group-hover:text-pink-600 transition-colors" />
-              <span className="relative inline-block after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
+              <FaInstagram className="text-xl my-1 group-hover:text-pink-600 transition-colors" />
+              <span className="hidden md:inline-block relative inline-block after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
                 Instagram
               </span>
             </Link>
@@ -72,8 +72,8 @@ const Footer = () => {
               rel="noopener noreferrer"
               className="group flex items-center gap-2 text-lg"
             >
-              <FaFacebook className="text-xl group-hover:text-blue-600 transition-colors" />
-              <span className="relative inline-block after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
+              <FaFacebook className="text-xl my-1 group-hover:text-blue-600 transition-colors" />
+              <span className="hidden md:inline-block relative inline-block after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
                 Facebook
               </span>
             </Link>
@@ -85,16 +85,16 @@ const Footer = () => {
               rel="noopener noreferrer"
               className="group flex items-center gap-2 text-lg"
             >
-              <FaTwitter className="text-xl group-hover:text-blue-400 transition-colors" />
-              <span className="relative inline-block after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
+              <FaTwitter className="text-xl my-1 group-hover:text-blue-400 transition-colors" />
+              <span className="hidden md:inline-block relative inline-block after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
                 Twitter
               </span>
             </Link>
           </div>
         </nav>
 
-        <nav className="flex flex-col gap-3">
-          <span className="pb-2 text-white/70 text-base font-medium">CODING PROFILES</span>
+        <nav className="flex flex-col gap-3 md:block hidden">
+          <span className="pb-3 text-linear-gradient(90deg, #edecec, #ffffff)/70 text-base font-medium">CODING PROFILES</span>
           <div>
             <Link
               href="https://leetcode.com/u/Wizard_a14/"
@@ -105,7 +105,7 @@ const Footer = () => {
               <span className="after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
                 Leetcode
               </span>
-              <span className="text-white/60 mb-0.5">- 50+ Problems Solved</span>
+              <span className="text-linear-gradient(90deg, #edecec, #ffffff)/60 mb-0.5">- 50+ Problems Solved</span>
             </Link>
           </div>
           <div>
@@ -118,7 +118,7 @@ const Footer = () => {
               <span className="after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
                 Geeksforgeeks
               </span>
-              <span className="text-white/60 mb-0.5">- 30+ Problems Solved</span>
+              <span className="text-linear-gradient(90deg, #edecec, #ffffff)/60 mb-0.5">- 30+ Problems Solved</span>
             </Link>
           </div>
           <div>
@@ -131,13 +131,13 @@ const Footer = () => {
               <span className="after:block after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
                 HackerRank
               </span>
-              <span className="text-white/60 mb-0.5">- 5 stars on C++, Python</span>
+              <span className="text-linear-gradient(90deg, #edecec, #ffffff)/60 mb-0.5">- 5 stars on C++, Python</span>
             </Link>
           </div>
         </nav>
 
         <div className="flex flex-col gap-2">
-          <span className="text-white/70 text-base font-medium">CONTACT</span>
+          <span className="text-linear-gradient(90deg, #edecec, #ffffff)/70 text-base font-medium">CONTACT</span>
           <div>
             <Link
               href="mailto:aniketsahaworkspace@gmail.com"
